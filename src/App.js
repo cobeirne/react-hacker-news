1baceacc071545a0032af9e17ca8d72b70409d5f
Chris OBeirne <chris.obeirne@onyxdigital.com.au>
2018-09-23 21:37:41 +1000

    Interactions with Forms and Events

--- src/App.js
+++ src/App.js
@@ -20,6 +20,11 @@ const list = [
   },
 ];
 
+function isSearched(searchTerm){
+  return function (item){
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
 
 class App extends Component {
   constructor(props){
@@ -27,21 +32,29 @@ class App extends Component {
 
     this.state = {
       list: list,
+      searchTerm:"",
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id){
     const updatedList = this.state.list.filter(item => item.objectID !== id);
     this.setState({list:updatedList});
   }
-  
-  render() {
-    const hellowWorld = 'Welcome to the Road to learn React';
-    return (
+ 
+  onSearchChange(event){
+    this.setState({searchTerm:event.target.value});
+  }
+
+  render(){
+    return(
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input type="text" onChange={this.onSearchChange}></input>
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -55,7 +68,7 @@ class App extends Component {
           </div>
         )}
       </div>
-    );
+    )
   }
 }
 
